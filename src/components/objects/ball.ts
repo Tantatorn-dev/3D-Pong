46810b795729be1ae2f1179eb21fc95288ceac0a
Tantatorn <timefear007@gmail.com>
2019-07-14 01:56:13 +0700

    add color to a ball

--- src/components/objects/ball.ts
+++ src/components/objects/ball.ts
@@ -1,10 +1,19 @@
-import { MeshBuilder, Vector3, Mesh, Scene } from "babylonjs";
+import { MeshBuilder, Vector3, Mesh, Scene, Material, StandardMaterial, Color3 } from "babylonjs";
 
 export default class Ball {
     _position: Vector3;
     _body: Mesh;
 
     constructor(scene: Scene) {
+        // create a ball
         this._body = MeshBuilder.CreateSphere("ball", { diameter: 10, updatable: true }, scene);
+
+        // prepare the material
+        let mat = new StandardMaterial("ballMaterial", scene);
+        mat.diffuseColor = new Color3(1, 0, 0);
+
+        // add color to a ball
+        this._body.material = mat;
     }
+
 };
